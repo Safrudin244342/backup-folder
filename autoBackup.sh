dbbcc18a2f194a7f7b7f14215f2176b28648de9c
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-09-25 08:30:49 +0700

    remove function folder mapping

--- autoBackup.sh
+++ autoBackup.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
+# tar -zcf 123.tar.gz -C /usr/log/backend . -C /usr/log/nginx .
 # args
 pathFrom=$1
 pathTo=$2
 
 files=()
 
-function fileMapping {
-  for file in $1/*
-  do
-    if [ -d "$file" ]
-    then
-      fileMapping $file
-    else
-      files[${#files[@]}]=$file
-      echo $file
-    fi
-  done
-}
-
 function start {
   if [ ${#pathFrom} != 0 ] && [ ${#pathTo} != 0 ] 
   then
-    fileMapping $pathFrom
+    # tar -zcf $pathTo -C $pathFrom ./
+    text=$(backupList.txt)
+    echo $text
   else
     echo "error"
   fi
