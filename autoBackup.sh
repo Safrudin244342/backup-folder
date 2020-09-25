c221418288efe087089d61d18a4e8b2cf8ca7f54
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-09-25 09:22:41 +0700

    make function compress folder

--- autoBackup.sh
+++ autoBackup.sh
@@ -2,17 +2,43 @@
 
 # tar -zcf 123.tar.gz -C /usr/log/backend . -C /usr/log/nginx .
 # args
-pathFrom=$1
-pathTo=$2
+IFS=' '
+read -a args <<< $@
 
 files=()
 
-function start {
+function readFile {
+  filename=$1
+  while IFS= read -r line
+  do
+    IFS='>'
+    read -a paths <<< "$line"
+    for path in "${paths[@]}"
+    do
+      echo $path
+    done
+  done < "$filename"
+}
+
+function compressFolder {
+  pathFrom=$1
+  pathTo=$2
+
   if [ ${#pathFrom} != 0 ] && [ ${#pathTo} != 0 ] 
   then
-    # tar -zcf $pathTo -C $pathFrom ./
-    text=$(backupList.txt)
-    echo $text
+    tar -zcf "$pathTo" -C "$pathFrom" .
+  else
+    echo "error"
+  fi
+}
+
+function start {
+  if [ "${args[0]}" = "-s" ]
+  then
+    compressFolder ${args[1]} ${args[2]}
+  elif [ "${args[0]}" = "-m" ]
+  then
+    echo "multi operation"
   else
     echo "error"
   fi
